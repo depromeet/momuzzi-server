3470af68fd24d7ffe1751208b0ec024696e2a393
parkmineum <a01087164428@gmail.com>
2025-08-26 01:45:30 +0900

    Initial Setting

---
+++ src/main/kotlin/org/depromeet/team3/Team3Application.kt
@@ -0,0 +1,11 @@
+package org.depromeet.team3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Team3Application
+
+fun main(args: Array<String>) {
+	runApplication<Team3Application>(*args)
+}
---
+++ src/test/kotlin/org/depromeet/team3/Team3ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.depromeet.team3
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Team3ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
