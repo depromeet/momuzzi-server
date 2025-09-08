bb7834dd75f605d6f811420577f3f32719f0db92
parkmineum <a01087164428@gmail.com>
2025-09-08 16:02:37 +0900

    WebConfigure 설정 추가

--- module-api/src/main/kotlin/org/depromeet/team3/config/SwaggerConfig.kt
+++ module-api/src/main/kotlin/org/depromeet/team3/config/SwaggerConfig.kt
@@ -5,9 +5,12 @@ import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class SwaggerConfig {
+class SwaggerConfig : WebMvcConfigurer {
 
     @Bean
     fun customOpenAPI(): OpenAPI {
@@ -18,8 +21,16 @@ class SwaggerConfig {
 
     private fun apiInfo(): Info {
         return Info()
-            .title("")
-            .description("")
+            .title("Team3 API")
+            .description("Team3 프로젝트 API 문서")
             .version("1.0")
+    }
+
+    override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer) {
+        configurer
+            .favorParameter(false)
+            .ignoreAcceptHeader(false)
+            .defaultContentType(MediaType.APPLICATION_JSON)
+            .mediaType("json", MediaType.APPLICATION_JSON)
     }
 }
