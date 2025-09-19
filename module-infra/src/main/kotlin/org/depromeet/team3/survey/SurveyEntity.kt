91ac9c2c8fcb7a703d43e7cfe33e7642cf08e396
Minji Kang <nitronium@ewhain.net>
2025-09-20 00:46:32 +0900

    feat: Survey 및 SurveyResult 도메인 모델 추가

---
+++ module-infra/src/main/kotlin/org/depromeet/team3/survey/SurveyEntity.kt
@@ -0,0 +1,34 @@
+package org.depromeet.team3.survey
+
+import jakarta.persistence.*
+import org.depromeet.team3.common.BaseTimeEntity
+import org.depromeet.team3.meeting.MeetingEntity
+import org.depromeet.team3.meetingattendee.MeetingAttendeeEntity
+import org.depromeet.team3.surveyresult.SurveyResultEntity
+
+@Entity
+@Table(
+    name = "tb_surveys",
+    uniqueConstraints = [
+        UniqueConstraint(
+            name = "uk_survey_meeting_participant",
+            columnNames = ["meeting_id", "participant_id"]
+        )
+    ]
+)
+class SurveyEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id", nullable = false)
+    val meeting: MeetingEntity,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "participant_id", nullable = false)
+    val participant: MeetingAttendeeEntity,
+
+    @OneToMany(mappedBy = "survey", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    val surveyResults: MutableList<SurveyResultEntity> = mutableListOf()
+) : BaseTimeEntity()
