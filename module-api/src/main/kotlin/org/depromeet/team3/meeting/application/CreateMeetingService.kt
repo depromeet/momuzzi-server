b6a28dec282c0a4bf6dc0a13e17dca9241eae23f
Minji Kang <nitronium@ewhain.net>
2025-10-24 00:13:27 +0900

    refactor: CreateMeetingService 코드 정리
    - 불필요한 import 정리 및 코드 포맷팅 개선

--- module-api/src/main/kotlin/org/depromeet/team3/meeting/application/CreateMeetingService.kt
+++ module-api/src/main/kotlin/org/depromeet/team3/meeting/application/CreateMeetingService.kt
@@ -1,9 +1,12 @@
 package org.depromeet.team3.meeting.application
 
+import org.depromeet.team3.auth.UserRepository
+import org.depromeet.team3.common.exception.ErrorCode
 import org.depromeet.team3.meeting.Meeting
 import org.depromeet.team3.meeting.MeetingRepository
 import org.depromeet.team3.meeting.dto.request.CreateMeetingRequest
 import org.depromeet.team3.meeting.dto.response.CreateMeetingResponse
+import org.depromeet.team3.meeting.exception.MeetingException
 import org.depromeet.team3.meetingattendee.MeetingAttendee
 import org.depromeet.team3.meetingattendee.MeetingAttendeeRepository
 import org.depromeet.team3.meetingattendee.MuzziColor
@@ -14,7 +17,8 @@ import org.springframework.transaction.annotation.Transactional
 class CreateMeetingService(
     private val meetingRepository: MeetingRepository,
     private val meetingAttendeeRepository: MeetingAttendeeRepository,
-    private val inviteTokenService: InviteTokenService
+    private val inviteTokenService: InviteTokenService,
+    private val userRepository: UserRepository
 ) {
 
     @Transactional
@@ -32,13 +36,22 @@ class CreateMeetingService(
         )
 
         val savedMeeting = meetingRepository.save(meeting)
-        val meetingId = savedMeeting.id!!
+        val meetingId = savedMeeting.id ?: throw IllegalStateException("Meeting ID is null")
+
+        // 사용자 정보 조회하여 닉네임 가져오기
+        val user = userRepository.findById(userId)
+            .orElseThrow { 
+                MeetingException(
+                    errorCode = ErrorCode.USER_NOT_FOUND,
+                    detail = mapOf("userId" to userId)
+                )
+            }
 
         val meetingAttendee = MeetingAttendee(
             id = null,
-            meetingId = savedMeeting.id!!,
+            meetingId = meetingId,
             userId = userId,
-            attendeeNickname = null,
+            attendeeNickname = user.nickname,
             muzziColor = MuzziColor.DEFAULT,
             createdAt = null,
             updatedAt = null
