a659477d9c694ac421aad8360404294b4bb94abe
dae won <eodnjs01477@gmail.com>
2025-10-27 01:15:14 +0900

    refactor: 참여한 모임 응답 수정

--- module-api/src/main/kotlin/org/depromeet/team3/meeting/application/GetMeetingService.kt
+++ module-api/src/main/kotlin/org/depromeet/team3/meeting/application/GetMeetingService.kt
@@ -2,27 +2,29 @@ package org.depromeet.team3.meeting.application
 
 import org.depromeet.team3.meeting.MeetingRepository
 import org.depromeet.team3.meeting.dto.response.MeetingInfoResponse
+import org.depromeet.team3.meeting.dto.response.MeetingsResponse
 import org.depromeet.team3.station.StationRepository
+import org.depromeet.team3.survey.application.GetSurveyListService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class GetMeetingService(
     private val meetingRepository: MeetingRepository,
-    private val stationRepository: StationRepository
+    private val stationRepository: StationRepository,
+    private val getSurveyListService: GetSurveyListService
 ) {
 
     @Transactional(readOnly = true)
-    operator fun invoke(userId: Long): List<MeetingInfoResponse> {
+    operator fun invoke(userId: Long): List<MeetingsResponse> {
         val meetings = meetingRepository.findMeetingsByUserId(userId)
         val stationIds = meetings.mapNotNull { it.stationId }.distinct()
         val stationMap = stationRepository.findAllById(stationIds)
             .associateBy { it.id }
 
-        return meetings.map { meeting ->
+        val meetingsResponse = meetings.map { meeting ->
             val stationName = stationMap[meeting.stationId]?.name ?: ""
-
-            MeetingInfoResponse(
+            val meetingInfo = MeetingInfoResponse(
                 id = meeting.id!!,
                 title = meeting.name,
                 hostUserId = meeting.hostUserId,
@@ -33,8 +35,19 @@ class GetMeetingService(
                 createdAt = meeting.createdAt!!,
                 updatedAt = meeting.updatedAt
             )
-        }
-    }
 
-    
+            val participantList = try {
+                getSurveyListService.getRespondents(meeting.id!!)
+            } catch (e: Exception) {
+                emptyList()
+            }
+            
+            MeetingsResponse(
+                meetingInfo = meetingInfo,
+                participantList = participantList
+            )
+        }
+
+        return meetingsResponse
+    }
 }
